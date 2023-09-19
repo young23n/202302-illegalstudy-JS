8bd77658235b134b098255c08c44a97067ef2200
유다원 <young23n@naver.com>
2023-09-19 18:46:59 +0900

    1week-2

---
+++ 1주차 2회차/13.js
@@ -0,0 +1,13 @@
+var a = '1';
+console.log(+a, typeof +a);
+console.log(a, typeof a);
+a = true;
+console.log(+a, typeof +a);
+console.log(a, typeof a);
+a = false;
+console.log(+a, typeof +a);
+console.log(a, typeof a);
+a = 'Hi';
+console.log(+a, typeof +a);
+console.log(a, typeof a);
+
---
+++ 1주차 2회차/18.js
@@ -0,0 +1,11 @@
+/*
+특강 진행도와 입력을 배우지 않은 것을 고려하여 문제를 선택했습니다.
+입력은 변수로 표현하겠습니다.
+https://www.acmicpc.net/problem/2739
+2739 구구단
+*/
+var input = 7;//입력
+
+for(var i = 1; i < 10; i++){
+    console.log(input+' * '+ i +' = '+(input*i));
+}
